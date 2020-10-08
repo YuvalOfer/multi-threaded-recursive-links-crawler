22b872e9a2e1d247891027286ec39eb085c00dfd
unknown <yuvalofer1@gmail.com>
2020-10-08 13:49:43 +0300

    refactor thread callbacks and smaller functions

--- LinksCrawler/crawler.py
+++ LinksCrawler/crawler.py
@@ -1,6 +1,5 @@
 import logging
-import time
-from queue import Queue
+from queue import Queue, Empty
 import requests
 import threading
 import concurrent.futures
@@ -8,7 +7,18 @@ from bs4 import BeautifulSoup
 
 from LinksCrawler import config
 
+
 LOGGER = logging.getLogger('Crawler')
+
+
+class URLD:
+    def __init__(self, url, depth):
+        self.url = url
+        self.depth = depth
+
+    @classmethod
+    def get_url_d(cls, url, depth):
+        return cls(url, depth)
 
 
 class Crawler:
@@ -19,113 +29,78 @@ class Crawler:
         self.q = Queue()
         self.url_dict = dict()
         self.broken_links = set()
+        self.scraped_pages = set()
         self.init_url = init_url
         self.thread_count = thread_count
         self.crawling_depth = crawling_depth
         self.logger = logger
-        self._dict_lock = threading.Lock()
         self.initialize_crawler()
 
     def initialize_crawler(self):
         """
         putting the initial link in the queue
         """
+        self._dict_lock = threading.Lock()
         self.url_dict[self.init_url] = 0
-        self.q.put(self.init_url)
+        self.q.put(URLD(self.init_url, 0))
         self.logger.info(f"queue initialized with link: {self.init_url}")
 
     def run(self):
-        """
-        initlize all threads and return the result
-        :return: tuple of dictionary and list
-        """
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.thread_count) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.thread_count) as self.pool:
+            while True:
+                try:
+                    urld = self.q.get(timeout=30)
+                    self.scraped_pages.add(urld.url)
+                    job = self.pool.submit(self.scrape_page, urld)
+                    job.add_done_callback(self.post_scrape_callback)
+                    self.q.task_done()
+                except Empty:
+                    break
+                except Exception as e:
+                    print(e)
+                    continue
 
-            for i in range(self.thread_count):
-                executor.submit(self.crwal_thread)
-                time.sleep(3)
-
-        self.q.join()
+            self.q.join()
         return self.url_dict, self.broken_links
 
-    def crwal_thread(self):
-        """
-        take a link from the queue, find its depth, process it
-        """
-        while not self.q.empty():
-
-            print(threading.active_count())
-            url = self.q.get()
-            self.logger.info(f'working on url: {url}')
-            url_depth = self.url_dict.get(url)
-            self.process_url(url, url_depth)
-            self.q.task_done()
-            self.logger.info(f"finished working on {url}")
-
-    def process_url(self, url, url_depth):
-        """
-        get all links in the page and if valid and non existent add to dict,
-        if broken add to list
-        :param url: str
-        :param url_depth: int
-        """
+    def scrape_page(self, urld):
         try:
-            for new_url in self.get_urls_from_url(url):
-                new_url_depth = url_depth + 1
-                valid_url = self.check_valid(new_url)
-                with self._dict_lock:
-                    dict_new_url_depth = self.url_dict.get(new_url, -1)
-                    if dict_new_url_depth != -1: # url was already found
-                        self.logger.info(f"{new_url} already exists in th dict")
-                        if new_url_depth < dict_new_url_depth:
-                            self.logger.warning(f"the new depth {new_url_depth} is better than {dict_new_url_depth}, updating")
-                            self.url_dict[new_url] = new_url_depth
-                        continue
+            response = requests.get(urld.url)
+            if response.status_code not in config.BAD_STATUS_CODES:
+                return response, urld.url, urld.depth
+            else:
+                return response, urld.url, -1
+        except requests.RequestException:
+            return
 
-                    elif valid_url:
-                        self.url_dict[new_url] = new_url_depth
-                        self.logger.info(f"{new_url} was added to the dict with value {new_url_depth}")
+    def post_scrape_callback(self, res):
+        response, url, url_depth = res.result()
+        if url_depth != -1:
+            if url_depth < self.crawling_depth:
+                self.insert_sub_url_to_q(response, url, url_depth)
+            self.update_dict(url, url_depth)
+        else:
+            self.broken_links.add(url)
 
-                    else:
-                        self.broken_links.add(new_url)
-                        self.logger.info(f"{new_url} is a broken and was added to the list")
+    def update_dict(self, url, url_depth):
+        with self._dict_lock:
+            dict_url_depth = self.url_dict.get(url, -1)
+            if dict_url_depth != -1:  # url was already found
+                self.logger.info(f"{url} already exists in th dict")
+                if url_depth < dict_url_depth:
+                    self.logger.warning(f"the new depth {url_depth} is better than {dict_url_depth}, updating")
+                    self.url_dict[url] = url_depth
 
-                if new_url_depth < self.crawling_depth and valid_url:
-                    self.q.put(new_url)
-                    self.logger.info(f"{new_url} was inserted to queue with depth: {new_url_depth}")
+            else:
+                self.url_dict[url] = url_depth
+                self.logger.info(f"{url} was added to the dict with value {url_depth}")
 
-        except Exception as e:
-            self.logger.warning(f'caught and unexpected exception while processing new_url: {url}')
-
-    @staticmethod
-    def get_urls_from_url(url):
-        """
-        return list of links from given utl
-        :param url: str
-        :return: list
-        """
-        response = requests.get(url=url, timeout=10)
-        soup = BeautifulSoup(response.text, 'lxml')
-        links = []
+    def insert_sub_url_to_q(self, request_response, url, url_depth):
+        soup = BeautifulSoup(request_response.text, 'lxml')
         for link in soup.find_all('a'):
             href = link.get('href')
             if href and href.startswith('http'):
                 if href.startswith('/'):
                     href = url + href
-                links.append(href)
-        return set(links)
-
-
-    @staticmethod
-    def check_valid(url):
-        """
-        check if link is broken
-        :param url: str
-        :return: bool
-        """
-        response = requests.get(url)
-        if response.status_code in config.BAD_STATUS_CODES:
-            return False
-        return True
-
-
+                if href not in self.scraped_pages:
+                    self.q.put(URLD.get_url_d(href, url_depth + 1))
