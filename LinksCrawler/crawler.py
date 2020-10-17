ed286157d3affb9a646cd2c79c76ffc8b473abc3
unknown <yuvalofer1@gmail.com>
2020-10-17 12:06:45 +0300

    type hints and exception log changes

--- LinksCrawler/crawler.py
+++ LinksCrawler/crawler.py
@@ -4,6 +4,7 @@ import requests
 import threading
 import concurrent.futures
 from bs4 import BeautifulSoup
+from typing import Tuple, Dict, Set
 
 from LinksCrawler import config
 
@@ -12,6 +13,9 @@ LOGGER = logging.getLogger('Crawler')
 
 
 class URLD:
+    """
+    Info holder class to prevent additional access to the url-depth dict while extracting a link from the queue
+    """
     def __init__(self, url, depth):
         self.url = url
         self.depth = depth
@@ -46,7 +50,11 @@ class Crawler:
         self.q.put(URLD(self.init_url, 0))
         self.logger.info(f"queue initialized with link: {self.init_url}")
 
-    def run(self):
+    def run(self) -> Tuple[Dict, Set]:
+        """
+        Main function of the crawler, extracts a link from the queue, initiates scraping and callback
+
+        """
         with self.pool:
             while True:
                 try:
@@ -57,25 +65,30 @@ class Crawler:
                     self.q.task_done()
                 except Empty:
                     break
-                except Exception as e:
-                    print(e)
-                    continue
+                except Exception:
+                    self.logger.exception("Caught an unexpected exception in the main loop")
 
             self.q.join()
         return self.url_dict, self.broken_links
 
-    @staticmethod
-    def scrape_page(urld):
+    def scrape_page(self, urld: URLD) -> Tuple[requests.models.Response, str, int]:
+        """
+        Gets the page content and passes it with the urld to the callback
+        """
         try:
             response = requests.get(urld.url)
             if response.status_code not in config.BAD_STATUS_CODES:
                 return response, urld.url, urld.depth
             else:
                 return response, urld.url, -1
-        except requests.RequestException:
-            return
+        except requests.ConnectionError:
+            self.logger.warning(f"There was a connection error to {urld.url}")
+            raise
 
-    def post_scrape_callback(self, res):
+    def post_scrape_callback(self, res: concurrent.futures.Future) -> None:
+        """
+        The post I/O function, calls for sub-links insertion if crawling depth is not reached and results dict update
+        """
         response, url, url_depth = res.result()
         if url_depth != -1:
             if url_depth < self.crawling_depth:
@@ -84,7 +97,10 @@ class Crawler:
         else:
             self.broken_links.add(url)
 
-    def update_dict(self, url, url_depth):
+    def update_dict(self, url: str, url_depth: int) -> None:
+        """
+        updates the result dict with lock protection to prevent bugs of simultaneous update for a given link
+        """
         with self._dict_lock:
             dict_url_depth = self.url_dict.get(url, -1)
             if dict_url_depth != -1:  # url was already found
@@ -97,7 +113,10 @@ class Crawler:
                 self.url_dict[url] = url_depth
                 self.logger.info(f"{url} was added to the dict with value {url_depth}")
 
-    def insert_sub_url_to_q(self, request_response, url, url_depth):
+    def insert_sub_url_to_q(self, request_response: requests.models.Response, url: str, url_depth: int) -> None:
+        """
+        parses the response and iterates over the links in the page, inserts the relevant ones as URLD to the queue
+        """
         soup = BeautifulSoup(request_response.text, 'lxml')
         for link in soup.find_all('a'):
             href = link.get('href')
